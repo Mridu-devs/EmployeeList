f321194cc9e11e636ade17d7c681c7aaac9f426e
Mridu-devs <mridu.devs@gmail.com>
2023-05-30 17:02:55 +0530

    header component created

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import AntIcon from 'react-native-vector-icons/AntDesign';
 
 export default function Header() {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <AntIcon name="pluscircleo" size={40} color={'white'} />
+        <TouchableOpacity>
+          <AntIcon name="pluscircleo" size={40} color={'white'} />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -14,6 +22,11 @@ export default function Header() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgba(8, 86, 58, 0.876)',
-    height: 100,
+    width: '100%',
+    padding: 20,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
   },
 });
